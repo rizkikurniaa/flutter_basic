3443172c210f6700db07abb060dc489c670a7290
Rizki <rizkikurniawan1797@gmail.com>
2021-01-22 14:28:58 +0700

    Implemented SingleChildScrollView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,76 +8,101 @@ class HomePage extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Profile Picture',
-                style: TextStyle(
-                    color: primaryColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset('assets/images/primary.png', width: 140),
-              SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Rizki Kurniawan',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                'Mobile Developer',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: greyColor,
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
                 ),
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Wrap(
-                spacing: 38,
-                runSpacing: 40,
-                children: [
-                  Image.asset(
-                    'assets/images/item1.png',
-                    width: 80,
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset('assets/images/primary.png', width: 140),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Rizki Kurniawan',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                   ),
-                  Image.asset(
-                    'assets/images/item2.png',
-                    width: 80,
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Mobile Developer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: greyColor,
                   ),
-                  Image.asset(
-                    'assets/images/item3.png',
-                    width: 80,
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'assets/images/item1.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item2.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item3.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item4.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item5.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item6.png',
+                      width: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  width: 224,
+                  height: 55,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    color: whiteColor,
+                    onPressed: () {},
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ),
-                  Image.asset(
-                    'assets/images/item4.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item5.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item6.png',
-                    width: 80,
-                  ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 76,
+                )
+              ],
+            ),
           ),
         ),
       ),
